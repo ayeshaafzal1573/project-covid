47abd27f9e59ce04c0b79961bcad386fb53d299b
ayeshaafzal1573 <ayeshaafzal1573@gmail.com>
2023-09-28 00:33:54 +0500

    contact updated

--- contact.php
+++ contact.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
-<!-- Website - www.codingnepalweb.com -->
-<html lang="en" dir="ltr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <title>Pandemix</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="icon" href="images/covidlogo.png">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-        media="screen">
-    <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
     <link rel="stylesheet" href="css/contact.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -22,7 +17,7 @@
         <div class="right">
             <a href="register.php"><i class="fa fa-user" aria-hidden="true"></i></a>
         </div>
-        <div class="container-fluid">
+        <div class="container">
             <div class="row d_flex">
                 <div class="col-sm-3 logo_sm">
                     <div class="logo">
@@ -48,8 +43,8 @@
             </div>
         </div>
     </header>
-
-    <div class="container">
+    <!-- HEADER END -->
+    <div class="container contact">
         <div class="content">
             <div class="left-side">
                 <div class="address details">
@@ -93,71 +88,66 @@
         </div>
     </div>
 
-    <!--  footer -->
-    <footer>
-        <div class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-2 col-md-6 col-sm-6">
-                        <div class="hedingh3 text_align_left">
-                            <h3>USEFUL LINKS</h3>
-                            <ul class="menu_footer">
-                                <li><a href="patient.php">Home</a>
-                                <li>
-                                <li><a href="myappointment.php">My Appointment</a>
-                                <li>
-                                <li> <a href="bookappointment.php">Book Appointment</a>
-                                <li>
-                                <li> <a href="covidreport.php">My Reports</a>
-                                <li>
+   <!--  footer -->
+   <footer>
+      <div class="footer">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-2 col-md-3 col-sm-6">
+                  <div class="hedingh3 text_align_left">
+                     <h3>USEFUL LINKS</h3>
+                     <ul class="menu_footer">
+                        <li><a href="index.php">Home</a>
+                        <li>
+                        <li><a href="index.php#about">About</a>
+                        <li>
+                        <li> <a href="contact.php">Contact</a>
+                        <li>
 
-                            </ul>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-3 col-sm-6">
+                  <div class="hedingh3 text_align_left">
+                     <h3>About</h3>
+                     <p>
+                        We are committed to delivering the latest news, health guidelines, and resources from reputable
+                        sources.
+                     </p>
+                  </div>
+               </div>
+               <div class="col-lg-3 col-md-3 col-sm-6">
+                  <div class="hedingh3  text_align_left">
+                     <h3>Contact Us</h3>
+                     <ul class="top_infomation">
+                        <li><i class="fa fa-phone" aria-hidden="true"></i>
+                           <a href="tel:111-222-333">+92-343-209872</a>
+                        </li>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i>
+                           <a href="mailto:ayeshaafzal1573@gmail.com">info@pandemix.com</a>
+                        </li>
+                        <li> <i class="fa fa-map-marker" aria-hidden="true"></i>
+                           <a href="https://www.google.com/maps/@24.8847152,67.1775322,15z?entry=ttu">Pandemix
+                              Hospital,Karachi</a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-3 col-sm-6">
+                  <div class="hedingh3 text_align_left">
+                     <h3>Location</h3>
+                     <div class="map">
+                        <img src="images/map.png" alt="map" />
+                     </div>
+                  </div>
+               </div>
 
-
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <div class="hedingh3 text_align_left">
-                            <h3>About</h3>
-                            <p>
-                                We are committed to delivering the latest news, health guidelines, and resources from
-                                reputable
-                                sources.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <div class="hedingh3  text_align_left">
-                            <h3>Contact Us</h3>
-                            <ul class="top_infomation">
-                                <li><i class="fa fa-phone" aria-hidden="true"></i>
-                                    <a href="tel:111-222-333">+92-343-209872</a>
-                                </li>
-                                <li><i class="fa fa-envelope" aria-hidden="true"></i>
-                                    <a href="mailto:ayeshaafzal1573@gmail.com">info@pandemix.com</a>
-                                </li>
-                                <li> <i class="fa fa-map-marker" aria-hidden="true"></i>
-                                    <a href="https://www.google.com/maps/@24.8847152,67.1775322,15z?entry=ttu">Pandemix
-                                        Hospital,Karachi</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="hedingh3 text_align_left">
-                            <h3>Location</h3>
-                            <div class="map">
-                                <img src="../images/map.png" alt="map" />
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
             </div>
+         </div>
 
-        </div>
-    </footer>
-    <!-- end footer -->
+      </div>
+   </footer>
+   <!-- end footer -->
 </body>
 
 </html>
