f0d418942a258a9a9cf8bcf280673191b3a2d330
ayeshaafzal1573 <ayeshaafzal1573@gmail.com>
2023-09-29 21:18:35 +0500

    done

--- project_covid.sql
+++ project_covid.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2023 at 06:14 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project_covid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
 
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
@@ -6,8 +34,18 @@ CREATE TABLE `admin` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `admin`
+--
+
 INSERT INTO `admin` (`admin_id`, `username`, `email`, `password`) VALUES
-(20, 'Admin', 'admin@gmail.com', 'admin');
+(1, 'Admin', 'admin@gmail.com', '$2y$10$H5Fu2fu.wUa2.FN/iURDuuXKyD838SaDCDHQ6KDnuqLhU/N8Ff0Ga');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
 
 CREATE TABLE `appointment` (
   `app_id` int(11) NOT NULL,
@@ -20,9 +58,20 @@ CREATE TABLE `appointment` (
   `approval_status` enum('Pending','Approved','Rejected') DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `appointment`
+--
 
 INSERT INTO `appointment` (`app_id`, `patient_id`, `hospital_id`, `app_date`, `status`, `app_time`, `test_name`, `approval_status`) VALUES
-(5, 9, 1, '2023-09-20', 1, '17:12:00', 'PCR', 'Approved');
+(1, 1, 1, '2023-09-30', 1, '19:25:00', 'PCR', 'Approved'),
+(2, 1, 1, '2023-09-30', 1, '12:46:00', 'Naats', 'Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital`
+--
+
 CREATE TABLE `hospital` (
   `hospital_id` int(11) NOT NULL,
   `hospital_name` varchar(255) DEFAULT NULL,
@@ -32,8 +81,19 @@ CREATE TABLE `hospital` (
   `approval_status` enum('Pending','Approved','Rejected') DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `hospital`
+--
+
 INSERT INTO `hospital` (`hospital_id`, `hospital_name`, `location`, `password`, `status`, `approval_status`) VALUES
-(1, 'aghakhan', 'Karachi', 'aghakhan', 1, 'Approved');
+(1, 'Agha Khan', 'Karachi', '$2y$10$MpkQ97qiwEi36mTzuhipcO8j6Gilcu5.Fu7bA6ND3djFGg0rM3NNm', 0, 'Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
 CREATE TABLE `patient` (
   `patient_id` int(11) NOT NULL,
   `patient_name` varchar(255) DEFAULT NULL,
@@ -42,14 +102,37 @@ CREATE TABLE `patient` (
   `password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `patient`
+--
+
 INSERT INTO `patient` (`patient_id`, `patient_name`, `address`, `email`, `password`) VALUES
-(8, 'aisha', 'shamsi', 'aisha@gmail.com', 'aisha');
+(1, 'Ayesha', 'Shamsi Society ', 'ayeshaafzal1573@gmail.com ', '$2y$10$mjCOLKl/RhACptOmCsbmN.wXXXLhwbeBEIeFReM1yDftdkBCzdaUi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
 CREATE TABLE `report` (
   `report_id` int(11) NOT NULL,
   `patient_id` int(11) DEFAULT NULL,
-  `test_result` varchar(50) DEFAULT NULL,
   `vac_suggest` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`report_id`, `patient_id`, `vac_suggest`) VALUES
+(1, 1, 'Booster');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccination`
+--
 
 CREATE TABLE `vaccination` (
   `vac_id` int(11) NOT NULL,
@@ -59,55 +142,123 @@ CREATE TABLE `vaccination` (
   `vac_status` enum('Available','Unavailable') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `vaccination`
+--
+
 INSERT INTO `vaccination` (`vac_id`, `vac_name`, `patient_id`, `hospital_id`, `vac_status`) VALUES
-(4, 'pi fizar', 9, 1, 'Available');
+(1, 'Booster', 1, 1, 'Available');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
-=
+
+--
+-- Indexes for table `appointment`
+--
 ALTER TABLE `appointment`
   ADD PRIMARY KEY (`app_id`),
   ADD KEY `patient_id` (`patient_id`),
   ADD KEY `hospital_id` (`hospital_id`);
 
+--
+-- Indexes for table `hospital`
+--
 ALTER TABLE `hospital`
   ADD PRIMARY KEY (`hospital_id`);
 
+--
+-- Indexes for table `patient`
+--
 ALTER TABLE `patient`
   ADD PRIMARY KEY (`patient_id`);
 
+--
+-- Indexes for table `report`
+--
 ALTER TABLE `report`
   ADD PRIMARY KEY (`report_id`),
   ADD KEY `patient_id` (`patient_id`);
 
+--
+-- Indexes for table `vaccination`
+--
 ALTER TABLE `vaccination`
   ADD PRIMARY KEY (`vac_id`),
   ADD KEY `patient_id` (`patient_id`),
   ADD KEY `hospital_id` (`hospital_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT for table `appointment`
+--
 ALTER TABLE `appointment`
-  MODIFY `app_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `app_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
+--
+-- AUTO_INCREMENT for table `hospital`
+--
 ALTER TABLE `hospital`
-  MODIFY `hospital_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `hospital_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT for table `patient`
+--
 ALTER TABLE `patient`
-  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT for table `report`
+--
 ALTER TABLE `report`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT for table `vaccination`
+--
 ALTER TABLE `vaccination`
-  MODIFY `vac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `vac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
 ALTER TABLE `appointment`
   ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`),
   ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`);
+
+--
+-- Constraints for table `report`
+--
 ALTER TABLE `report`
   ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`);
 
+--
+-- Constraints for table `vaccination`
+--
 ALTER TABLE `vaccination`
   ADD CONSTRAINT `vaccination_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`),
   ADD CONSTRAINT `vaccination_ibfk_2` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
